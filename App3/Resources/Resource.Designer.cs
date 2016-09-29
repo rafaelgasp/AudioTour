8c4b7f5e0c375fb73c701ed15213fe9e9685a035
Rafael Gaspar <rafaelgaspar@hotmail.com.br>
2016-09-29 17:42:11 -0300

    Added fields: Location and Date to New User Screen

--- App3/Resources/Resource.Designer.cs
+++ App3/Resources/Resource.Designer.cs
@@ -78,44 +78,53 @@ namespace AudioTour
 			// aapt resource value: 0x7f060000
 			public const int ButtonMain = 2131099648;
 			
-			// aapt resource value: 0x7f06000d
-			public const int btnPlay = 2131099661;
+			// aapt resource value: 0x7f060010
+			public const int btnPlay = 2131099664;
 			
-			// aapt resource value: 0x7f06000c
-			public const int btnStop = 2131099660;
+			// aapt resource value: 0x7f06000f
+			public const int btnStop = 2131099663;
+			
+			// aapt resource value: 0x7f06000d
+			public const int gridLayout1 = 2131099661;
 			
 			// aapt resource value: 0x7f06000a
-			public const int gridLayout1 = 2131099658;
-			
-			// aapt resource value: 0x7f060007
-			public const int imageButton1 = 2131099655;
+			public const int imageButton1 = 2131099658;
 			
 			// aapt resource value: 0x7f060002
 			public const int linearLayout1 = 2131099650;
 			
-			// aapt resource value: 0x7f06000b
-			public const int relativeLayout1 = 2131099659;
-			
-			// aapt resource value: 0x7f060009
-			public const int seekBar1 = 2131099657;
-			
 			// aapt resource value: 0x7f06000e
-			public const int space1 = 2131099662;
+			public const int relativeLayout1 = 2131099662;
 			
-			// aapt resource value: 0x7f060008
-			public const int space2 = 2131099656;
+			// aapt resource value: 0x7f06000c
+			public const int seekBar1 = 2131099660;
+			
+			// aapt resource value: 0x7f060011
+			public const int space1 = 2131099665;
+			
+			// aapt resource value: 0x7f06000b
+			public const int space2 = 2131099659;
 			
 			// aapt resource value: 0x7f060003
 			public const int textView1 = 2131099651;
 			
 			// aapt resource value: 0x7f060005
-			public const int txtEmail = 2131099653;
+			public const int textView2 = 2131099653;
+			
+			// aapt resource value: 0x7f060007
+			public const int textView3 = 2131099655;
+			
+			// aapt resource value: 0x7f060006
+			public const int txtEmail = 2131099654;
+			
+			// aapt resource value: 0x7f060008
+			public const int txtLocation = 2131099656;
 			
 			// aapt resource value: 0x7f060004
 			public const int txtName = 2131099652;
 			
-			// aapt resource value: 0x7f060006
-			public const int txtOut = 2131099654;
+			// aapt resource value: 0x7f060009
+			public const int txtOut = 2131099657;
 			
 			static Id()
 			{
@@ -177,8 +186,14 @@ namespace AudioTour
 			// aapt resource value: 0x7f050000
 			public const int ButtonMain = 2131034112;
 			
+			// aapt resource value: 0x7f050006
+			public const int NewUserDateOfBirth = 2131034118;
+			
 			// aapt resource value: 0x7f050005
 			public const int NewUserEmail = 2131034117;
+			
+			// aapt resource value: 0x7f050007
+			public const int NewUserLocation = 2131034119;
 			
 			// aapt resource value: 0x7f050004
 			public const int NewUserName = 2131034116;
